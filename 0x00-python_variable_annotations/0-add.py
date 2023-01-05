5fc0d841af9d8da10a0acb86fb3326d8af63b3ba
OPunga <ronnyopunga96@gmail.com>
2023-01-06 00:10:38 +0300

    add-annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""module"""
+
+def add(a: float, b: float) -> float:
+    """
+    Takes two floating point numbers as arguments
+    where
+    a: first flaoting point number
+    b: second floating point number
+    """
+    return a+b
